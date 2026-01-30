7b15627c3e7cb88ebc303fd32d72db7a6f3fb970
Lukas J Han <lukas.j.han@gmail.com>
2026-01-30 19:37:30 +0100

    feat: add guard in provider

--- registrar-be/src/modules/auth/auth.module.ts
+++ registrar-be/src/modules/auth/auth.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
+import { JwtGuard } from './jwt.guard';
 
 @Module({
   imports: [UserModule],
-  providers: [AuthService],
-  exports: [AuthService],
+  providers: [AuthService, JwtGuard],
+  exports: [AuthService, JwtGuard],
   controllers: [AuthController],
 })
 export class AuthModule {}
