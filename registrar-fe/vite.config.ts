15c6c9a0b32b2dee4b415369aacf038a3515fee8
Lukas J Han <lukas.j.han@gmail.com>
2026-01-31 14:01:27 +0100

    feat: restore vit config

--- registrar-fe/vite.config.ts
+++ registrar-fe/vite.config.ts
@@ -3,12 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/auth': 'http://localhost:18000',
-      '/wrp': 'http://localhost:18000',
-      '/status-management': 'http://localhost:18000',
-    },
-  },
 })
